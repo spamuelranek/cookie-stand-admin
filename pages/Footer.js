73dde043a8939539eae7f28f5003752addd66935
Samuel Panek <samuelrpanek@gmail.com>
2022-01-20 18:06:14 -0800

    adds basic form and functionality

---
+++ pages/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className=' bg-emerald-500 p-3'>
+      <h3 className='text-xs text-gray-800'>©2022</h3>
+    </footer>
+  )
+}
---
+++ pages/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className=' bg-emerald-500 '>
+      <h1 className=' text-2xl px-4 py-1 '>Cookie Stand Admin</h1>
+    </header>
+  )
+}
