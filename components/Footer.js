e4d6a46c3e724754697423389725153497b0f03f
Samuel Panek <samuelrpanek@gmail.com>
2022-01-24 21:53:03 -0800

    adds footer functionality

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,21 @@
+import useResource from "../hooks/useResource"
+
 export default function Footer() {
+  const { resources } = useResource()
+  
+  function AmountOfStores(){
+    if (resources && resources.length > 0) {
+      return resources.length
+    }
+    else{
+      return 0
+    }
+  }
+
+
   return (
     <footer className=' bg-emerald-500 p-3'>
-      <h3 className='text-xs text-gray-800'>©2022</h3>
+      <h3 className='text-xs text-gray-800'> There is {AmountOfStores()} stores open in the world</h3>
     </footer>
   )
 }
